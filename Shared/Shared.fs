d687c85dfb790bcea02dcfd5062d936e56bc3b30
Ohad Frenkel Keshet <ohadfrenkel@outlook.com>
2025-08-28 09:24:13 +0300

    Add support for rides
    - Added the `Rides` module defining the domain type for a ride.
    - The `Ride`'s constructor is private forcing creation via the constructor function.
    - Added a **shared** `PositiveNonZeroInt` type, with private constructor, to force certain numerical values to be strictly positive.

--- Shared/Shared.fs
+++ Shared/Shared.fs
@@ -1,1 +1,22 @@
 ﻿namespace Shared
+
+[<AutoOpen>]
+module Domain =
+    [<Measure>]
+    type cm
+
+    [<Measure>]
+    type yr
+
+    type PositiveNonZeroInt<[<Measure>] 'u> = private PositiveNonZeroInt of int<'u>
+
+    module PositiveNonZeroInt =
+        let create x =
+            if x > 0<_> then Some(PositiveNonZeroInt x) else None
+
+        let value (PositiveNonZeroInt x) = x
+
+    type Tags =
+        | FamilyFriendly
+        | Thrilling
+        | Educational
---
+++ src/server/FunPark/Rides.fs
@@ -0,0 +1,45 @@
+namespace FunPark
+
+open System
+open Shared
+open FSharp.Data.UnitSystems.SI.UnitSymbols
+
+module Rides =
+    type RideStatus =
+        | Online
+        | Offline
+
+    type Ride =
+        private
+            { Id: Guid
+              Name: string
+              MinAge: int<yr>
+              MinHeight: int<cm>
+              WaitTime: int<s>
+              Online: RideStatus
+              Tags: Tags list }
+
+    module Ride =
+        type RideConstructor =
+            { Name: string
+              MinAge: PositiveNonZeroInt<yr> option
+              MinHeight: PositiveNonZeroInt<cm> option
+              WaitTime: PositiveNonZeroInt<s> option
+              Online: RideStatus
+              Tags: Tags list }
+
+        let create
+            { Name = name
+              MinAge = minAge
+              MinHeight = minHeight
+              WaitTime = waitTime
+              Online = online
+              Tags = tags }
+            =
+            { Id = Guid.NewGuid()
+              Name = name
+              MinAge = defaultArg (Option.map PositiveNonZeroInt.value minAge) 10<yr>
+              MinHeight = defaultArg (Option.map PositiveNonZeroInt.value minHeight) 90<cm>
+              WaitTime = defaultArg (Option.map PositiveNonZeroInt.value waitTime) 30<s>
+              Online = online
+              Tags = tags }
