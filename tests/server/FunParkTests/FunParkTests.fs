11fc7adf7eb5c21755e52de26c2c37cde31ee880
Ohad Frenkel Keshet <ohadfrenkel@outlook.com>
2025-08-29 11:57:33 +0300

    Initial test efforts
    - Using Expecto as both the test organizer and runner.
    - Initial effort, two tests only: happy flow, invlid parameter.
    - Tests are still in same file as test runner trigger (i.e., the `[<EntryPoint>]`).
    - Expand...

--- tests/server/FunParkTests/FunParkTests.fs
+++ tests/server/FunParkTests/FunParkTests.fs
@@ -1,1 +1,38 @@
-﻿namespace FunParkTests
+﻿open Expecto
+open Shared
+open FunPark.Rides
+open FSharp.Data.UnitSystems.SI.UnitSymbols
+
+module DomainTests =
+    let ridesTests =
+        testList
+            "Rides tests"
+            [ testCase "Create a ride with all valid parameters"
+              <| fun _ ->
+                  let ride =
+                      Ride.create
+                          { Name = "Roller Coaster"
+                            MinAge = PositiveNonZeroInt.create 8<yr>
+                            MinHeight = PositiveNonZeroInt.create 90<cm>
+                            WaitTime = PositiveNonZeroInt.create 25<s>
+                            Online = Some Online
+                            Tags = [ FamilyFriendly; Thrilling ] }
+
+                  Expect.equal (Ride.value ride).Name "Roller Coaster" "Expected ride name to match"
+
+              testCase "Create a ride with (at least one) invalid parameter(s)"
+              <| fun _ ->
+                  let ride =
+                      Ride.create
+                          { Name = "Invalid Ride"
+                            MinAge = PositiveNonZeroInt.create -88<yr>
+                            MinHeight = PositiveNonZeroInt.create 90<cm>
+                            WaitTime = PositiveNonZeroInt.create 25<s>
+                            Online = Some Online
+                            Tags = [ FamilyFriendly; Thrilling ] }
+
+                  Expect.equal (Ride.value ride).MinAge 8<yr> "Expected ride min age to match" ]
+
+[<EntryPoint>]
+let main args =
+    runTestsWithCLIArgs [] args DomainTests.ridesTests
