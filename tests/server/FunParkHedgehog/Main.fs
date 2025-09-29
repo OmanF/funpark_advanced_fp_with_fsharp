9ab8bf848c7c4faa48feca77aa63ddeea070fb5b
Ohad Frenkel Keshet <ohadfrenkel@outlook.com>
2025-09-29 16:44:47 +0300

    Refactor test execution to streamline property checks for Ride attributes
    - Minimized code while retaining clarity about test execution, and failure in case of.

--- tests/server/FunParkHedgehog/Main.fs
+++ tests/server/FunParkHedgehog/Main.fs
@@ -3,24 +3,24 @@ open FunParkHedgehog.Tests
 
 [<EntryPoint>]
 let main _ =
-    printfn "Testing: All Ride's MinAge is positive"
-    let minAgeResult = Property.check propMinAgeNatural
-    printfn "%A" minAgeResult
+    let testsNames =
+        [ "MinAgeNatural"
+          "MinHeightNatural"
+          "MinWaitTimeNatural"
+          "NoDuplicateTags"
+          "OfflineWaitTimeZero" ]
 
-    printfn "Testing: All Ride's MinHeight is positive"
-    let minHeightResult = Property.check propMinHeightNatural
-    printfn "%A" minHeightResult
+    let testsFunctions =
+        [ propMinAgeNatural
+          propMinHeightNatural
+          propMinWaitTimeNatural
+          propNoDuplicateTags
+          propOfflineWaitTimeZero ]
 
-    printfn "Testing: All Ride's MinWaitTime is positive"
-    let minWaitTimeResult = Property.check propMinWaitTimeNatural
-    printfn "%A" minWaitTimeResult
+    let testsZipped = List.zip testsNames testsFunctions
 
-    printfn "Testing: All Ride's Tags have no duplicates"
-    let noDuplicateTagsResult = Property.check propNoDuplicateTags
-    printfn "%A" noDuplicateTagsResult
-
-    printfn "Testing: All offline Ride's WaitTime is zero"
-    let offlineWaitTimeZeroResult = Property.check propOfflineWaitTimeZero
-    printfn "%A" offlineWaitTimeZeroResult
+    testsZipped
+    |> List.iter (fun (name, prop) ->
+        printfn $"Testing %s{name} (Unit result means no failures): %A{Property.check prop}")
 
     0
